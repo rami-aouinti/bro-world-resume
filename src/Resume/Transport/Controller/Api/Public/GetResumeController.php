e71edf9992acd54957453522f80bac24a3c87977
rami aouinti <rami.aouinti@gmail.com>
2025-11-03 00:44:27 +0100

    fix uuid

---
+++ src/Resume/Transport/Controller/Api/Public/GetResumeController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Resume\Transport\Controller\Api\Public;
+
+use App\General\Infrastructure\ValueObject\SymfonyUser;
+use App\Resume\Domain\Repository\ResumeRepositoryInterface;
+use OpenApi\Attributes as OA;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ReviewStatsController
+ *
+ * @package App\Tool\Transport\Controller\Api
+ * @author  Rami Aouinti <rami.aouinti@tkdeutschland.de>
+ */
+#[Route('/platform/resume', name: 'resume_api_platform_')]
+class GetResumeController extends AbstractController
+{
+
+
+    public function __construct(
+        private readonly ResumeRepositoryInterface $resumeRepository,
+    )
+    {
+    }
+
+    #[Route('/', name: 'get', methods: ['GET'])]
+    #[OA\Get(
+        summary: 'Get resume',
+    )]
+    public function __invoke(SymfonyUser $symfonyUser): JsonResponse
+    {
+        $resume = $this->resumeRepository->findOneByUserId($symfonyUser->getUserIdentifier());
+
+        return new JsonResponse([
+            'resume' => $resume
+        ]);
+    }
+}
